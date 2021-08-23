06c434ce04a9eb6755816dd1a0e322352775c5d2
Isaque Souza <isaquetoonix245@gmail.com>
2021-08-22 22:05:17 -0300

    [update] (style): created and styled Navigation component]

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { View } from 'react-native';
+import styled from 'styled-components/native';
+import { AntDesign, MaterialIcons } from '@expo/vector-icons';
+
+
+const Container = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;  
+  padding-top: 20px;
+`;
+
+const Button = styled.TouchableOpacity`
+  height: 40px;
+  width: 40px;
+  border-radius: 10px;
+  background-color: #00000040;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleContent = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  font-weight: 300;
+  font-size: 18px;
+  color: #4A4A4A;
+`;
+
+const Strong = styled.Text`
+  font-weight: 700;
+`;
+
+
+export default ({onPress, title, titleStrong} : any) => {
+  return(
+    <Container>
+      <Button onPress={onPress}>
+        <AntDesign name="arrowleft" size={24} color="white" />
+      </Button>
+
+      <TitleContent>
+        <Title>{title} <Strong>{titleStrong}</Strong> </Title>  
+      </TitleContent>
+
+      <Button>
+        <MaterialIcons name="menu" size={24} color="white" />
+      </Button>
+    </Container>
+  );
+}
