3071faa220c11432b0a8897c43f29d22765e86ad
Isaque Souza <isaquetoonix245@gmail.com>
2021-08-22 22:07:43 -0300

    [update] (style): [created and styled Advice screen]

--- src/screens/Advice/index.tsx
+++ src/screens/Advice/index.tsx
@@ -1,18 +1,80 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import styled from 'styled-components/native';
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
+import { Modalize } from 'react-native-modalize';
+import { BackgroundImage } from '../../../styles';
 
-// import { Container } from './styles';
+import Navigation from '../../components/Navigation';
+import PageCard from '../../components/PageCard';
 
-const Container = styled.View`
-  background-color: #CCC;
-  height: 100%;
-`;
+import Background from '../../assets/Advice/background.png'
+import iconPage from '../../assets/Advice/iconPage.png'
 
-export default function Advice(){
+import { 
+  Container,
+  ImagePage,
+  Title,
+  Content,
+  Wrapper,
+  Row
+} from '../../assets/Styles/PageTemplate/styles';
+
+export default function Localization({navigation}:any){
+
+  const modalizeRef = useRef<Modalize>(null);
+
   return(
-    <Container>
-      <Text> Advice </Text>
-    </Container>
+    <React.Fragment>
+      <BackgroundImage source={Background} resizeMode="cover" >
+        <Container>
+          <Navigation
+            onPress={() => navigation.goBack('history')}
+            title="Marcar"
+            titleStrong="Aviso"
+          />
+
+          <ImagePage>
+            <Image source={iconPage}/>
+          </ImagePage>
+
+        </Container>
+      </BackgroundImage>
+
+      <Modalize 
+        ref={modalizeRef}
+        adjustToContentHeight={false}
+        alwaysOpen={450}
+      >
+        <Title>Informe o tipo de aviso 🔮</Title>
+        <Content>
+          <Wrapper>
+            <Row>
+              <PageCard
+                badge={true}
+                text="Estação de trabalho"
+                backgroundColor={{backgroundColor: '#DCD2FF'}}
+                // onPress={}
+              />
+              <PageCard
+                text="Manutenção"
+                backgroundColor={{backgroundColor: '#DCD2FF'}}
+                // onPress={}
+              />
+            </Row>
+            <Row>
+              <PageCard
+                text="Limpeza"
+                backgroundColor={{backgroundColor: '#DCD2FF'}}
+                // onPress={}
+              />
+              <PageCard
+                text="Objeto"
+                backgroundColor={{backgroundColor: '#DCD2FF'}}
+                // onPress={}
+              />
+            </Row>
+          </Wrapper>
+        </Content>
+      </Modalize>
+    </React.Fragment>
   )
 }
