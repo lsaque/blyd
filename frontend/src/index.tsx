cf416c4fcdbd2dc0963371f08859f0eefe7b5086
Isaque Souza <isaquetoonix245@gmail.com>
2021-10-07 18:03:45 -0300

    [update] (routes): [implemented Advice top tab navigation and route]

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { ImageBackground, View } from 'react-native';
+import { BackgroundImage } from '../styles';
 
 import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 
@@ -20,12 +23,82 @@ import UserProfile from './screens/UserProfile';
 import UserList from './screens/CRUD/Admin/UserList';
 import UserEditProfile from './screens/CRUD/Admin/UserEditProfile';
 import UserCreateProfile from './screens/CRUD/Admin/UserCreateProfile';
+import Assigned from './screens/CRUD/Admin/AdviceList/Assigned'
+import Concluded from './screens/CRUD/Admin/AdviceList/Concluded'
+import Navigation from './components/Navigation';
 
+const AdviceTab = createMaterialTopTabNavigator();
 const UserTab = createMaterialBottomTabNavigator();
 const AdminTab = createMaterialBottomTabNavigator();
-
 const AdminStack = createStackNavigator();
 const UserStack = createStackNavigator();
+
+import Background from "./assets/Admin/background.png";
+
+function AdminAdviceTopTab({navigation}: any) {
+  return (
+    <React.Fragment>
+      <BackgroundImage 
+        source={ Background } 
+        resizeMode="cover" 
+        style={{
+          paddingHorizontal: 20,
+          height: 80
+        }}
+      >
+        <Navigation
+          title="Listar"
+          titleStrong="Avisos"
+          onPress={() => navigation.goBack('history')}
+          lightContent={true}
+        />
+      </BackgroundImage>
+
+      <AdviceTab.Navigator
+        initialRouteName='Assigned'
+        backBehavior='history'
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: '#fff',
+            // paddingHorizontal: 20,
+            // backgroundColor: '#f8f7f7',
+            // height: 50,
+          },
+          tabBarLabelStyle: {
+            textTransform: 'capitalize',
+            fontSize: 16,
+          },
+          tabBarIndicatorStyle: {
+            // backgroundColor: "#000",
+            backgroundColor: '#8363F6',
+          },
+          tabBarInactiveTintColor: "#a3a3a3",
+          tabBarActiveTintColor: "#8363F6",
+        }}
+        sceneContainerStyle={{
+          backgroundColor: "#fff"
+        }}
+      >
+        <AdviceTab.Screen 
+          name="Assigned" 
+          component={Assigned} 
+          options={{
+            tabBarLabel: "Atribuídos",
+          }}
+        />
+        <AdviceTab.Screen 
+          name="Concluded" 
+          component={Concluded} 
+          options={{
+            tabBarLabel: "Concluídos"
+          }}
+        />
+      </AdviceTab.Navigator>
+
+
+    </React.Fragment>
+  );
+}
 
 function UserBottomTab(){
   return(
@@ -229,6 +302,7 @@ const AdminScreen = () => {
       <AdminStack.Screen name="UserProfile" component={UserProfile}/>
       <AdminStack.Screen name="UserEditProfile" component={UserEditProfile}/>
       <AdminStack.Screen name="UserCreateProfile" component={UserCreateProfile}/>
+      <AdminStack.Screen name="AdviceList" component={AdminAdviceTopTab}/>
 
       <AdminStack.Screen name="Search" component={Search} />
       <AdminStack.Screen name="Localization" component={Localization} />
